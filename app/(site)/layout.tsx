a27d51bc70db6a628c43605a98693b71a804c7b2
feng <1416929275@qq.com>
2024-04-24 10:42:54 +0800

    fix: 设置布局的验证时间

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import { BackToTop } from '@/components/back-to-top';
 import { Footer } from '@/components/footer';
 import { Navbar } from '@/components/navbar';
 
+export const revalidate = 60;
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <>
